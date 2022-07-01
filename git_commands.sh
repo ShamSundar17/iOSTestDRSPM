aea264b7f3bc9f6e65c902ff7b39c34925e673c1
Sham Sundar <Sham.Sundar@costrategix.com>
2022-07-01 10:50:28 +0530

    Update the SDK for version: 4.4.2

--- git_commands.sh
+++ git_commands.sh
@@ -17,7 +17,8 @@ done
 git add .
 git commit -m "$MESSAGE" --quiet
 git push -u origin main --quiet
-#git tag -a $VERSION -m "v$VERSION"
+git tag -a $VERSION -m "v$VERSION"
+git push --tags
 gh release create $VERSION ../Digita1Reef/DigitalReefiOS/build/SDK_$VERSION.zip
 
 
